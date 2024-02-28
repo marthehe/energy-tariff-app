4fbbbab4411cb132e371bc670d86c6490aa6ce67
Marta Hendel <marta.hendel@gmail.com>
2024-02-28 15:01:09 +0000

    Adding Style and Theme

--- tariffapp/components/MainNav.tsx
+++ tariffapp/components/MainNav.tsx
@@ -1,12 +1,19 @@
 import Link from "next/link";
 import React from "react";
+import ToggleMode from "./ToggleMode";
 
 const MainNav = () => {
   return (
-    <div>
-      <Link href="/">Dashboard</Link>
-      <Link href="/tariffs">Tariffs</Link>
-      <Link href="/users">Users</Link>
+    <div className="flex justify-between">
+      <div className="flex items-center gap-2">
+        <Link href="/">Dashboard</Link>
+        <Link href="/tariffs">Tariffs</Link>
+        <Link href="/users">Users</Link>
+      </div>
+      <div className="flex items-center gap-2">
+        <Link href="/">Logout</Link>
+        <ToggleMode />
+      </div>
     </div>
   );
 };
