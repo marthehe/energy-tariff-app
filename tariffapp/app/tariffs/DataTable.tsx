7c41b3a8928b6ee73a8297904063f4e0a4b25639
Marta Hendel <marta.hendel@gmail.com>
2024-02-29 13:09:56 +0000

    Adding Tariff Table

---
+++ tariffapp/app/tariffs/DataTable.tsx
@@ -0,0 +1,56 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Tariff } from "@prisma/client";
+import React from "react";
+
+interface Props {
+  tariffs: Tariff[];
+}
+
+const DataTable = ({ tariffs }: Props) => {
+  console.log("hello world");
+  return (
+    <div className="w-full mt-5">
+      <div className="rounded-md sm:border">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Name</TableHead>
+              <TableHead>Price</TableHead>
+              <TableHead>Description</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Created At</TableHead>
+              <TableHead>Updated At</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {tariffs
+              ? tariffs.map((tariff) => (
+                  <TableRow key={tariff.id} data-href="/">
+                    <TableCell>{tariff.name} </TableCell>
+                    <TableCell>{tariff.price} </TableCell>
+                    <TableCell>{tariff.description} </TableCell>
+                    <TableCell>{tariff.Status} </TableCell>
+                    <TableCell>
+                      {tariff.createdAt.toLocaleDateString()}{" "}
+                    </TableCell>
+                    <TableCell>
+                      {tariff.updatedAt.toLocaleDateString()}{" "}
+                    </TableCell>
+                  </TableRow>
+                ))
+              : null}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
+};
+
+export default DataTable;
--- tariffapp/app/tariffs/page.tsx
+++ tariffapp/app/tariffs/page.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import prisma from "@/prisma/db";
+import DataTable from "./DataTable";
 
 const Tariffs = async () => {
   const tariffs = await prisma.tariff.findMany();
 
   console.log(tariffs);
-  return <div>Tariffs</div>;
+  return (
+    <div>
+      <DataTable tariffs={tariffs} />
+    </div>
+  );
 };
 
 export default Tariffs;
