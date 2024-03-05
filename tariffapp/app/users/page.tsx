cf5d4f0e20fab0c26984a0411aaf0a1324b8695e
Marta Hendel <marta.hendel@gmail.com>
2024-03-05 15:27:36 +0000

    Adding User Form

--- tariffapp/app/users/page.tsx
+++ tariffapp/app/users/page.tsx
@@ -1,7 +1,15 @@
+import UserForm from "@/components/UserForm";
 import React from "react";
+import prisma from "@/prisma/db";
 
-const Users = () => {
-  return <div>Users</div>;
+const Users = async () => {
+  const users = await prisma.user.findMany();
+
+  return (
+    <div>
+      <UserForm />
+    </div>
+  );
 };
 
 export default Users;
